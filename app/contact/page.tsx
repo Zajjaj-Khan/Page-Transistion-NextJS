98468ffe7bc9159774dd7495ab6436078104b605
Zajjaj-Khan <kzajjaj@gmail.com>
2024-05-28 13:38:28 +0500

    Page transitions in Next js

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return (
+      <section className='py-24'>
+        <div className='container'>
+          <h1 className='mb-12 text-3xl font-bold'>Contact Details</h1>
+          <p className="max-w-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas ratione, iste animi aliquid quae hic at, officiis rerum, deleniti quo? Facere, veniam! Possimus at libero cum rem nulla cupiditate?
+          </p>
+        </div>
+      </section>
+    )
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h1 className='mb-12 text-3xl font-bold'>Page Transistions</h1>
+        <p className="max-w-lg">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas ratione, iste animi aliquid quae hic at, officiis rerum, deleniti quo? Facere, veniam! Possimus at libero cum rem nulla cupiditate?
+        </p>
       </div>
     </section>
   )
